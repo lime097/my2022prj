84c784c779ca6a3d0a6ea45253e5d0d3e2426041
jmj <ccab1470@gmail.com>
2022-04-07 15:06:34 +0900

    commit

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,5 +1,9 @@
 public class hello {
     public static void main(String[] args){
         System.out.println("HELLO5");
-    }
+        public static void main(String[] args){
+            System.out.println("HELLO5");
+
+
+        }
 }
