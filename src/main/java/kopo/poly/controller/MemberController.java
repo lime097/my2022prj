8e94e3a1468a8e2745f08a6fe725d9f62291853a
jmj <ccab1470@gmail.com>
2022-04-21 10:51:06 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/kopo/poly/controller/MemberController.java
    #	src/main/java/kopo/poly/service/impl/MemberService.java

--- src/main/java/kopo/poly/controller/MemberController.java
+++ src/main/java/kopo/poly/controller/MemberController.java
@@ -2,6 +2,9 @@ package kopo.poly.controller;
 
 import kopo.poly.service.IMemberService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.mail.internet.MimeMessage;
+import java.util.Random;
 
 
 /*
@@ -24,6 +29,8 @@ import javax.annotation.Resource;
 public class MemberController {
     @Resource(name = "MemberService")
     private IMemberService memberService;
+    @Autowired
+    private JavaMailSender mailSender;
 
     /**
      * GetMapping은 GET방식을 통해 접속되는 URL 호출에 대해 실행되는 함수로 설정함을 의미함
@@ -62,4 +69,47 @@ public class MemberController {
         }
 
     } // memberIdChkPOST() 종료
+    /* 이메일 인증 */
+    @RequestMapping(value="/mailCheck", method=RequestMethod.GET)
+    @ResponseBody
+    public String mailCheckGET(String email) throws Exception{
+
+        /* 뷰(View)로부터 넘어온 데이터 확인 */
+
+
+        /* 인증번호(난수) 생성 */
+        Random random = new Random();
+        int checkNum = random.nextInt(888888) + 111111;
+
+
+        /* 이메일 보내기 */
+        String setFrom = "1820100293@gspace.kopo.ac.kr";
+        String toMail = email;
+        String title = "회원가입 인증 이메일 입니다.";
+        String content =
+                "홈페이지를 방문해주셔서 감사합니다." +
+                        "<br><br>" +
+                        "인증 번호는 " + checkNum + "입니다." +
+                        "<br>" +
+                        "해당 인증번호를 인증번호 확인란에 기입하여 주세요.";
+
+        try {
+
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
+            helper.setFrom(setFrom);
+            helper.setTo(toMail);
+            helper.setSubject(title);
+            helper.setText(content,true);
+            mailSender.send(message);
+
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        String num = Integer.toString(checkNum);
+
+        return num;
+
+    }
 }
